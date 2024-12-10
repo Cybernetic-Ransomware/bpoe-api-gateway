462d6671e2cefbfc202bf51dd4cde127cf156afd
Cybernetic-Ransomware <71835339+Cybernetic-Ransomware@users.noreply.github.com>
2024-12-10 10:16:32 +0100

    Hotfix: Annotated

--- src/auth/router.py
+++ src/auth/router.py
@@ -1,3 +1,5 @@
+from typing import Annotated
+
 from fastapi import APIRouter, Depends, Security
 from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
 
@@ -10,11 +12,14 @@ from src.auth.utils import RoleVerifier
 router = APIRouter()
 token_auth_scheme = HTTPBearer()
 
-def verify_roles(token: HTTPAuthorizationCredentials = Depends(token_auth_scheme),  # noqa: B008
-                 allowed_roles: list[str | None] = None) -> bool:
+def verify_roles(
+        token: Annotated[HTTPAuthorizationCredentials,Depends(token_auth_scheme)],
+        allowed_roles: list[str | None] = None) -> bool:
     verifier = RoleVerifier(token, allowed_roles)
     return verifier.verify()
 
 @router.get("/priv", dependencies=[Depends(auth.implicit_scheme)])
-async def get_private(user: Auth0User = Security(auth.get_user), valid_roles: bool = Depends(verify_roles)):  # noqa: B008
+async def get_private(
+        user: Annotated[Auth0User, Security(auth.get_user)],
+        valid_roles: Annotated[bool, Depends(verify_roles)]) -> dict[str, str]:
     return {"message": f"Hello World but in prvate {user.id}, {user.email}"}
