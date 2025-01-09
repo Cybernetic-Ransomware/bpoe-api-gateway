6e3c79066c6f171ad9fe44282fefb289ee138780
Cybernetic-Ransomware <71835339+Cybernetic-Ransomware@users.noreply.github.com>
2025-01-09 19:29:25 +0100

    Synch - cookie workflow

--- src/auth/router.py
+++ src/auth/router.py
@@ -1,6 +1,7 @@
 from typing import Annotated
 
-from fastapi import APIRouter, Depends, Security, Response, Request
+from fastapi import APIRouter, Depends, Security, Response, Request, Query
+from fastapi.params import Query
 from fastapi.responses import RedirectResponse
 from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
 from fastapi_auth0 import Auth0User
@@ -9,9 +10,8 @@ from httpx import AsyncClient
 from src.auth.config import auth
 from src.auth.exceptions import ExchangeTokenException
 from src.auth.utils import RoleVerifier
-from src.auth.models import TokenRequest
+# from src.auth.models import TokenRequest
 from src.config import AUTH0_DOMAIN, AUTH0_CLIENT_SECRET, APP_CLIENT_ID, APP_ALLLOWED_CALLBACK_URL
-
 
 router = APIRouter()
 token_auth_scheme = HTTPBearer()
@@ -43,37 +43,43 @@ async def get_private(
         valid_roles: Annotated[bool, Depends(verify_roles)]) -> dict[str, str]:
     return {"message": f"Hello World but in prvate {user.id}, {user.email}"}
 
-@router.post("cookie")
-async def cookie_callback(request: Request, response: Response):
-    token = request.session.get("access_token")
-    if not token:
-        raise ExchangeTokenException(code=500)
-    response.set_cookie(key="auth_token", value="token", max_age=3600, httponly=True, secure=False)
-    return{"message": "Session cookie set"}
+# @router.post("cookie")
+# async def cookie_callback(request: Request, response: Response):
+#     print("Authentication in progress", flush=True)
+#     token = request.session.get("access_token")
+#     if not token:
+#         raise ExchangeTokenException(code=500)
+#     response.set_cookie(key="auth_token", value="token", max_age=3600, httponly=True, secure=False)
+#     print("Authenticated", flush=True)
+#     return{"message": "Session cookie set"}
 
-@router.post("/exchange-token")
-async def exchange_token(request: TokenRequest):
+@router.get("/exchange-token")
+async def exchange_token(request: Request, code: str = Query(..., description="Exchange Token")):
     token_url = f"https://{AUTH0_DOMAIN}/oauth/token"
     async with AsyncClient() as client:
-        res  = await client.post(
+        res = await client.post(
             token_url,
             json={
                 "grant_type": "authorization_code",
                 "client_id": APP_CLIENT_ID,
                 "client_secret": AUTH0_CLIENT_SECRET,
-                "code": request.auth_code,
+                "code": code,
                 # "redirect_uri": "http://localhost:8080/log/cookie",  # + endpoint /callback; zrób redirect na fron; request.session (by nie przechowywaćw localstorage, a w cookie tworzonym przez backend)
-                "redirect_uri": "http://127.0.0.1:8080/log/cookie",
+                # "redirect_uri": "http://127.0.0.1:8080/docs",
+                "redirect_uri": "http://127.0.0.1:8070/index.html",
             },
         )
-        print(res.status_code, flush=True)
-        print(res.text, flush=True)
+        # print(res.status_code, flush=True)
+        # print(res.text, flush=True)
 
+        print(res, flush=True)
         if res.status_code != 200:
+            print(res.text, flush=True)
             raise ExchangeTokenException(res.status_code)
 
         token_data = res.json()
         access_token = token_data.get("access_token")
+        print(access_token, flush=True)
         if not access_token:
             raise ExchangeTokenException(code=500)
 
--- src/main.py
+++ src/main.py
@@ -1,8 +1,12 @@
 from fastapi import FastAPI
+from starlette.middleware.sessions import SessionMiddleware
 
 from src.auth.router import router as auth_router
 app = FastAPI()
+app.add_middleware(SessionMiddleware, secret_key="some-random-string")
+
 app.include_router(auth_router, prefix="/log", tags=["auth"])
+
 
 @app.get("/")
 async def root():
