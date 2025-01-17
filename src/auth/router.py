0a5e91cc8e660acb6721d0fc5919958ffbdd1947
Cybernetic-Ransomware <71835339+Cybernetic-Ransomware@users.noreply.github.com>
2025-01-17 15:28:38 +0100

    Fixed token type

--- src/auth/router.py
+++ src/auth/router.py
@@ -1,36 +1,13 @@
 from fastapi import APIRouter, Depends, Security, Request
 from fastapi.params import Query
-from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials, SecurityScopes
-from fastapi_auth0 import Auth0, Auth0User
+from fastapi.security import HTTPBearer
+from fastapi_auth0 import Auth0User
 from httpx import AsyncClient
 
-from src.auth.config import auth
-from src.auth.exceptions import ExchangeTokenException
-from src.auth.utils import RoleVerifier
-# from src.auth.models import TokenRequest
 from src.config import AUTH0_DOMAIN, AUTH0_CLIENT_SECRET, APP_CLIENT_ID, AUTH0_API_AUDIENCE
+from src.auth.exceptions import ExchangeTokenException
+from src.auth.utils import CustomAuth0
 
-
-class Auth0HTTPBearer(HTTPBearer):
-    async def __call__(self, request: Request):
-        return await super().__call__(request)
-
-class CustomAuth0(Auth0):
-    def __init__(self, domain: str, api_audience: str, scopes: dict = {}, **kwargs):
-        super().__init__(domain, api_audience, scopes, **kwargs)
-
-    async def get_user(self, security_scopes: SecurityScopes, creds: HTTPAuthorizationCredentials | None = Depends(Auth0HTTPBearer(auto_error=False)), raw_request: Request = None) -> Auth0User | None:
-        if creds is None:
-            access_token = raw_request.session.get("access_token") if raw_request else None
-            if not access_token:
-                access_token = raw_request.cookies.get("session") if raw_request else None
-
-            if not access_token:
-                raise ExchangeTokenException(code=403, verbose="Missing bearer token")
-
-            creds = HTTPAuthorizationCredentials(scheme="Bearer", credentials=access_token)
-
-        return await super().get_user(security_scopes, creds)
 
 router = APIRouter()
 token_auth_scheme = HTTPBearer()
@@ -46,7 +23,6 @@ async def get_private(
     request: Request,
     user: Auth0User = Security(authorizer.get_user)
 ) -> dict[str, str]:
-    scheme = authorizer.implicit_scheme
     user = user
     return {"message": f"Hello World {user.email} but in private."}
 
--- src/auth/utils.py
+++ src/auth/utils.py
@@ -1,100 +1,31 @@
-import jwt
+from fastapi import Depends, Request
+from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials, SecurityScopes
 
-from typing import Any
+from fastapi_auth0 import Auth0, Auth0User
 
-from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
-
-from src.auth.config import set_up
-from src.auth.exceptions import (TokenVerificationException, InvalidTokenSignatureException, TokenDecodeException,
-                                 InvalidClaimException, InsufficientClaimException, PermissionsException)
+from src.auth.exceptions import ExchangeTokenException
 
 
-token_auth_scheme = HTTPBearer()
+class Auth0HTTPBearer(HTTPBearer):
+    async def __call__(self, request: Request):
+        return await super().__call__(request)
 
+class CustomAuth0(Auth0):
+    def __init__(self, domain: str, api_audience: str, scopes: dict = None, **kwargs):
+        if scopes is None:
+            scopes = {}
 
-class VerifyToken:
-    def __init__(self, token: str, permissions: str | None = None, scopes: str | None = None, role: str | None = None):
-        self.config = set_up()
-        self.token = token
-        self.permissions = permissions
-        self.scopes = scopes
-        self.role = role
-        self.signing_key = None
+        super().__init__(domain, api_audience, scopes, **kwargs)
 
-        jwks_url = f'https://{self.config["DOMAIN"]}/.well-known/jwks.json'
-        self.jwks_client = jwt.PyJWKClient(jwks_url)
+    async def get_user(self, security_scopes: SecurityScopes, creds: HTTPAuthorizationCredentials | None = Depends(Auth0HTTPBearer(auto_error=False)), raw_request: Request = None) -> Auth0User | None:
+        if creds is None:
+            access_token = raw_request.session.get("access_token") if raw_request else None
+            if not access_token:
+                access_token = raw_request.cookies.get("session") if raw_request else None
 
-    def verify(self) -> {str: Any}:
-        try:
-            self.signing_key = self.jwks_client.get_signing_key_from_jwt(
-                self.token
-            ).key
-        except jwt.exceptions.PyJWKClientError as error:
-            raise TokenVerificationException(detail=str(error)) from error
-        except jwt.exceptions.DecodeError as error:
-            raise InvalidTokenSignatureException(detail=str(error)) from error
+            if not access_token:
+                raise ExchangeTokenException(code=403, verbose="Missing bearer token")
 
-        try:
-            payload = jwt.decode(
-                self.token,
-                self.signing_key,
-                algorithms=self.config["ALGORITHMS"],
-                audience=self.config["API_AUDIENCE"],
-                issuer=self.config["ISSUER"],
-            )
-        except jwt.exceptions.ExpiredSignatureError as error:
-            raise TokenDecodeException(detail="Token has expired.")  from error
-        except jwt.exceptions.InvalidAudienceError as error:
-            raise TokenDecodeException(detail="Invalid audience in token.") from error
-        except Exception as error:
-            raise TokenDecodeException(detail=str(error)) from error
+            creds = HTTPAuthorizationCredentials(scheme="Bearer", credentials=access_token)
 
-        if 'email' not in payload:
-            raise InvalidClaimException('email')
-
-        if self.scopes:
-            self._check_claims(payload, 'scope', str, self.scopes.split(' '))
-
-        if self.permissions:
-            self._check_claims(payload, 'permissions', list, self.permissions)
-
-        if self.role:
-            self._check_claims(payload, 'customField/roles', list, self.role)
-
-        return payload
-
-    @staticmethod
-    def _check_claims(payload: {str: Any}, claim_name: str, claim_type: Any, expected_value: [str]) -> {str: Any}:
-        if claim_name not in payload or not isinstance(payload[claim_name], claim_type):
-            raise InvalidClaimException(claim_name)
-
-        payload_claim = payload[claim_name]
-
-        if claim_name == 'scope':
-            payload_claim = payload[claim_name].split(' ')
-
-        if claim_name == 'customField/roles':
-            payload_claim = payload[claim_name]
-
-        for value in expected_value:
-            if value not in payload_claim:
-                raise InsufficientClaimException(claim_name, value)
-
-        return {"status": "success", "status_code": 200}
-
-
-class RoleVerifier:
-    def __init__(self, token: HTTPAuthorizationCredentials, allowed_roles: list[str] | None):
-        self.token = token
-        self.allowed_roles = allowed_roles
-
-    def verify(self) -> bool:
-        auth = VerifyToken(self.token.credentials).verify()
-
-        if not self.allowed_roles:
-            return True
-
-        if any(role in auth.get('customField/roles') for role in self.allowed_roles):
-            return True
-        else:
-            raise PermissionsException()
+        return await super().get_user(security_scopes, creds)
