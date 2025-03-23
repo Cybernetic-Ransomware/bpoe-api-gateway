9b9ee17b0f7ddb8800ce1dd8e527699b7ba814fa
Cybernetic-Ransomware <71835339+Cybernetic-Ransomware@users.noreply.github.com>
2025-03-23 21:49:11 +0100

    Sentry
    Sentry, sync

--- src/main.py
+++ src/main.py
@@ -1,3 +1,5 @@
+import sentry_sdk
+
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
@@ -5,6 +7,13 @@ from starlette.middleware.sessions import SessionMiddleware
 
 from src.auth.router import router as auth_router
 
+
+sentry_sdk.init(
+    dsn="https://49e6307f9d7c96aefb5e6bf8f308576f@o4509011278495745.ingest.de.sentry.io/4509011283935312",
+    send_default_pii=True,
+    traces_sample_rate=1.0,
+    profiles_sample_rate=1.0,
+)
 
 origins = [
     "http://localhost",
